272c6a527dd6416e815889385739969ec2f8245e
ismailhunaid <ismael552@gmail.com>
2020-12-01 23:24:51 +0500

    expense tracker app by travesy

--- class-6/src/App.js
+++ class-6/src/App.js
@@ -1,17 +1,28 @@
 import React from 'react'
 import Main from './Main'
+
+import {ProviderFunction} from './TransactionList'
 import './App.css';
 
-function App() {
-  return(
-    <div>
-      <h1> app component </h1>
 
-      <br />
-      < Main />
+
+function App() {
+  return (
+    
+    <div>
+       <ProviderFunction> 
+      
+      <h1> app component </h1>
+        <br />
+        < Main />
+        
+
+         </ProviderFunction> 
+      
+        
 
     </div>
   )
 }
-  
+
 export default App;
--- class-6/src/Main.js
+++ class-6/src/Main.js
@@ -1,39 +1,95 @@
 import React from 'react'
-import Form from './Form'
-import List from './List'
+//import Form from './Form'
+import { useContext, useState } from 'react'
+import { TransactionList } from './TransactionList'
 
-const Main = () => {
-    return (
-        <div>
-            <div className="container">
-
-                <h1 className="text-center"> <b> EXPENSE TRACKER APP  </b> </h1>
-
-                <br />
+function Main () {
+    let list = useContext(TransactionList)
+    let {addTransaction} = useContext(TransactionList)
 
 
-                <h3> YOUR BALANCE = dynamic  </h3>
+    let [description, setDescription] = useState("");
+    let [amount, setAmount] = useState(0);
+
+    function handleAddition(event) {
+        event.preventDefault()
+        console.log(addTransaction)
+        console.log(description, amount)
+        addTransaction({
+            
+            amount,
+            description
+        })
 
 
-                <div className="expense-container">
-
-                    <h3> INCOME <br />  <hr /> dynamic   </h3>
-
-                    <h3> EXPENSE <br /> <hr />  dynamic  </h3>
+    }
 
 
-                </div>
-                <h3> HISTORY</h3>
-                <hr />
-                <List />
-                <h3> ADD NEW TRANSACTIONS </h3>
 
-                <Form />
+return (
+    <div>
+
+        <div className="container">
+
+            <h1 className="text-center"> <b> EXPENSE TRACKER APP  </b> </h1>
+
+            <br />
+
+
+            <h3> YOUR BALANCE = dynamic  </h3>
+
+
+            <div className="expense-container">
+
+                <h3> INCOME <br />  <hr /> dynamic   </h3>
+
+                <h3> EXPENSE <br /> <hr />  dynamic  </h3>
+
 
             </div>
+            <h3> HISTORY</h3>
+            <hr />
+
+            <ul className="transaction-list">
+                {list.map((listObj, index) => {
+                    return (
+                        <li key={index}>
+                            <span>  {listObj.description}</span>
+                            <span> {listObj.amount}</span>
+                        </li>
+                    )
+                })}
+            </ul>
+
+
+
+
+
+
+
+            <h3> ADD NEW TRANSACTIONS </h3>
+
+            <form className="form" onSubmit={handleAddition}>
+
+                <label> ENTER DESCRIPTION</label> <br />
+                <input type="text" name=""  onChange={(e) => { setDescription(e.target.value) }} required />
+                <br />
+
+                <label> ENTER AMOUNT </label> <br />
+                <input type="number" name="" onChange={(e) => { setAmount(e.target.value) }} required />  <br />
+
+                <input type="submit" value="ADD TRANSACTION" />
+            </form>
+
+
+
+
+
 
         </div>
-    )
+
+    </div>
+)
 }
 
 
